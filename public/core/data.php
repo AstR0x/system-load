6ab467293e5b39f408a2a162947ddea9bd4397de
Vlad <astr0xy1@gmail.com>
2019-01-23 04:32:07 +0300

    added charts

--- public/core/data.php
+++ public/core/data.php
@@ -1,15 +1,15 @@
 <?php
-$cpu = shell_exec('ps aux | awk \'{s += $3} END {print s "%"}\'');
+$cpu = shell_exec('ps aux | awk \'{s += $3} END {print s }\'');
 $total_mem = shell_exec("cat /proc/meminfo | grep MemTotal | awk '{ print $2 }' ");
-$free_mem = shell_exec("cat /proc/meminfo | grep MemFree | awk '{ print $2 }' ");
+$free_mem = shell_exec("cat /proc/meminfo | grep Cached | awk '{ print $2 }' ");
 $average1 =  shell_exec("uptime |  awk '{ print $10 }' ");
 $average2 =  shell_exec("uptime |  awk '{ print $11 }' ");
 $average3 =  shell_exec("uptime | awk '{ print $12 }' ");
-$arr = array('CPI' => trim($cpu),
+$arr = array('CPU' => trim($cpu),
               'TotalMemory' => trim($total_mem),
               'FreeMemory' => trim($free_mem),
-              "5minute" => substr(trim($average1), 0, -1),
-              "10minute" => substr(trim($average2), 0, -1),
+              "1minute" => substr(trim($average1), 0, -1),
+              "5minute" => substr(trim($average2), 0, -1),
               "15minute" => substr(trim($average3), 0, -1));
 
 exit (json_encode($arr));
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,65 @@
+var cpuCtx = document.getElementById('cpuChart').getContext('2d');
+var memoryCtx = document.getElementById('memoryChart').getContext('2d');
+let time = performance.now();
+var cpuData = [];
+var memoryData = [];
+var timeData = [];
+
+
+var cpuChart = new Chart(cpuCtx, {
+  type: 'line',
+  data: {
+    labels: timeData,
+    datasets: [{
+      label: "CPU",
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: cpuData,
+    }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
+
+var memoryChart = new Chart(memoryCtx, {
+  type: 'line',
+  data: {
+    labels: timeData,
+    datasets: [{
+      label: "Free memory",
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: memoryData,
+    }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
+
+
 function App(props) {
   return (
-      <table className="system-load-table" width="100%">
-        <tbody>
+      <table className="system-load-table">
+        <tbod>
         <tr className="first_row">
           <th>CPU</th>
           <th>Total memory</th>
           <th>Free memory</th>
+          <th>1 minute</th>
           <th>5 minute</th>
-          <th>10 minute</th>
           <th>15 minute</th>
         </tr>
         <tr className="second-row">
-          <th>{props.data['CPI']}</th>
+          <th>{props.data['CPU']}</th>
           <th>{(props.data['TotalMemory'] / 1024).toFixed(2) + ' МБ'}</th>
           <th>{(props.data['FreeMemory'] / 1024).toFixed(2) + ' МБ'}</th>
+          <th>{props.data['1minute']}</th>
           <th>{props.data['5minute']}</th>
-          <th>{props.data['10minute']}</th>
           <th>{props.data['15minute']}</th>
         </tr>
-        </tbody>
+        </tbod>
       </table>
   );
 }
@@ -26,9 +67,14 @@ function App(props) {
 setInterval(function () {
   $.get('core/data.php', function (data) {
     data = JSON.parse(data);
+    cpuData.push(data['CPU'] / 1);
+    memoryData.push((data['FreeMemory'] / 1024).toFixed(2));
+    timeData.push(((performance.now() - time) / 1000).toFixed(2));
+    cpuChart.update();
+    memoryChart.update();
     ReactDOM.render(<App data={data}/>, document.getElementById('root'));
   });
-}, 500);
+}, 400);
 
 
 
